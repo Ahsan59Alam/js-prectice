08d6df3a25b28b0bc6c6157a8dc7164bdb04c0be
Ahsan Alam <124789001+Ahsan59Alam@users.noreply.github.com>
2024-03-10 20:47:23 +0000

    add variables

---
+++ basic/variable.js
@@ -0,0 +1,17 @@
+const accountId=144552
+let acccountEmail="ahsan@gmail.com"
+var accountPassword="12345"
+let accountState;
+
+
+
+accountCity="noida"
+
+// accountId=23 //not allowed
+
+acccountEmail= "alam@Email.com"
+accountPassword="212121"
+accountCity="ghaziabad"
+
+console.log(accountId);
+console.table([acccountEmail,accountPassword,accountId,accountCity,accountState])
